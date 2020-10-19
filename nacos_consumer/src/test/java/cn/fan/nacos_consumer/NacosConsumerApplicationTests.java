0019f843ce60299692a699ff1bdbb48b443734db
LightMoney <fan85719223@outlook.com>
2020-10-19 13:37:03 +0800

    nacos

---
+++ nacos_consumer/src/test/java/cn/fan/nacos_consumer/NacosConsumerApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.fan.nacos_consumer;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
