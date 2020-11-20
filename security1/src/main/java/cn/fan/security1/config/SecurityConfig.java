0a780b166304c7a53c9e58cd6aad7311a32e0951
LightMoney <fan85719223@outlook.com>
2020-11-20 17:43:41 +0800

    security测试

--- security1/src/main/java/cn/fan/security1/config/SecurityConfig.java
+++ security1/src/main/java/cn/fan/security1/config/SecurityConfig.java
@@ -1,10 +1,32 @@
 package cn.fan.security1.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //认证（采用哪种认证方式 jdbc memory。。）
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+
+    //rule defined
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // 设置拦截忽略文件夹，可以对静态资源放行
+        web.ignoring().antMatchers("templates/**", "/css/**", "/js/**");
+    }
 }
